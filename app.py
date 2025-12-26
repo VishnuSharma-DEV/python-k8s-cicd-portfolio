bed7ab15908b685e1f671aaea1627eadce8dfc19
Vishnu Sharma <vishnusharma06102000@gmail.com>
2025-12-26 19:44:25 +0530

    Modified the app file for commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Kubernetes CI/CD Pipeline!"
+    return f"Served from pod: {socket.gethostname()} — Deployed automatically by Jenkins CI/CD!"
 
 app.run(host="0.0.0.0", port=5000)
